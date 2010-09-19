85846d71714dc95a15de9be610711a00ec84f08d
Anton Pawlik <apaw@kupferwerk.net>
2010-09-19 19:45:17 +0200

    scroll notification setup

---
+++ auswertung/antpaw/js/scrollNotebar.js
@@ -0,0 +1,27 @@
+var scrollNotebar = new Class({
+	Implements: [Options],
+
+	options: {
+		offsets: { x:0, y:0 },
+		mode: 'vertical'
+	},
+
+	initialize: function(menu, options) {
+		this.setOptions(options);
+		
+		this.menu = $(menu);
+		this.move = this.options.mode == 'vertical' ? 'y' : 'x';
+		
+		window.addEvent('scroll', this.scrollListener.bind(this));
+		this.scrollListener();
+	},
+	
+	scrollListener: function() {
+		var marginTop = this.options.offsets[this.move] - $(document.body).getScroll()[this.move];
+		if (marginTop < 0) {
+			marginTop = 0;
+		}
+		
+		this.menu.setStyles({'margin-top': marginTop});
+	}
+});
